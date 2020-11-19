9a639d126336aeeef156df12aec8387f6befef7d
HM Edgar <hospmex.sistemas@gmail.com>
2020-11-19 12:40:08 -0800

    card login

--- src/components/auth/modules/LoginForm.tsx
+++ src/components/auth/modules/LoginForm.tsx
@@ -1,46 +1,54 @@
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Checkbox, Card } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import React from "react";
 
 const LoginForm = ({ onFinish }: any) => {
   return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="email"
-        rules={[{ required: true, message: "Ingrese su direccion de correo" }]}
+    <Card title="Hospital Mexico" bordered={false}>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
       >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Correo"
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: "Ingrese su contrasena" }]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Contrasena"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Recordarme</Checkbox>
+        <Form.Item
+          name="email"
+          rules={[
+            { required: true, message: "Ingrese su direccion de correo" },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Correo"
+          />
         </Form.Item>
-      </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Ingrese su contrasena" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Contrasena"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Recordarme</Checkbox>
+          </Form.Item>
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Acceder
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Acceder
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
   );
 };
 
